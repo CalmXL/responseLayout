323311fbacb12002b791488a709dfb43a67bd069
xulei <xl18845898715@gmail.com>
2022-07-08 08:46:28 +0800

    f

---
+++ Readme.md
@@ -0,0 +1,12 @@
+PC 
+1200px       card 25%
+
+1199px    100%  card 25%;
+
+768px     card 50%;
+          nav 消失  按钮  出现
+
+414px     iphone xsmax  手机端
+          html font0-size 动态设置
+
+ipad  ipad pro    Pc端 呈现
---
+++ js/common.js
@@ -0,0 +1,50 @@
+;(function (doc) {
+
+  var oMenuBtn = doc.querySelector('.header .fa-bars'),
+      oNav = doc.querySelector('.header .header-nav');
+
+  var init = function () {
+    setFontSize();
+    bindEvent();
+  }
+
+  function bindEvent () {
+    window.addEventListener('resize', setFontSize, false);
+    oMenuBtn.addEventListener('click', showNav, false);
+  }
+
+  function setFontSize () {
+    console.log(1);
+    var cWidth= doc.documentElement.clientWidth; 
+    console.log(cWidth);
+
+    if (cWidth <= 414) {
+      doc.documentElement.style.fontSize = cWidth / 37.5 + 'px';
+    } else {
+      const fontSize = doc.documentElement.style.fontSize;
+
+      if (fontSize !== '62.5%') {
+        doc.documentElement.style.fontSize = "62.5%"
+      }
+    }
+
+    if (cWidth > 768) {
+      oNav.style.display = 'block';
+    } else {
+      oNav.style.display = 'none';
+    }
+  }
+
+  function showNav () {
+    var isShow = oNav.style.display;
+
+    if (isShow === 'block') {
+      oNav.style.display = 'none';
+    } else {
+      oNav.style.display = 'block';
+    }
+  }
+
+
+  init();
+})(document);
